aacee7d61e0bc3d1198b5c296652d9fb64932bf7
Shivalika <shivalikakhajuria@gmail.com>
2025-04-19 01:57:08 +0530

    adding changes in body parser and using morgan in same file

--- Body Parser/index.js
+++ Body Parser/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bodyParser from "body-parser";
+import morgan from "morgan";
 
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -8,15 +8,16 @@ const __dirname = dirname(fileURLToPath (import.meta.url))
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded ( { extended : true }));
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    // res.sendFile(__dirname + "/public/index.html");
+    res.send("hello world");
 })
 
-app.post("/submit" , (req,res) => {
-     console.log(req.body)
-})
+// app.post("/submit" , (req,res) => {
+//      console.log(req.body)
+// })
 
 app.listen( port , () => {
     console.log(`Listening to Port ${port}`)
